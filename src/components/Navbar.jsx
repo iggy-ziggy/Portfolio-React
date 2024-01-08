657db4cdb35c0f8a2a5a9bc8d4c6f4167a83bfc3
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-01-08 17:07:40 -0500

    added collapsible functionality for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
-// import { Menu } from '../assets/images/menu.svg';
 import { useState } from 'react';
-import power from '../assets/images/power.png';
+import toggle from '../assets/images/toggle.png';
+import portfolioLogo from '../assets/images/portfolio-logo.png';
 import logo from '../assets/images/logo-mh-blue.png'
 
 export default function Nav({ links }) {
-    // const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(true);
     
-    // const toggleOpen = () => {
-    //     setOpen(!open);
-    // }
+    const toggleOpen = () => {
+        setOpen(!open);
+    }
 
-    
     return (
-        <nav className="nav">
-            <a href="/"><img className="nav__brand-image" src={logo} alt="" /></a>
-            <ul className="nav--list">
+        <nav className="nav collapsible">
+            <a href="/"><img className="nav__brand-image" src={portfolioLogo} alt="" /></a>
+            <img className='nav__toggler' onClick={toggleOpen} src={toggle} alt="" />
+            <ul className={open ? 'list nav__list collapsible__content collapsible--expanded':'list nav__list collapsible__content nav__list-closed'}>
                 {links.map((link) => link)}
             </ul>
+            {/* <ul className='list nav__list collapsible__content collapsible--expanded'>
+                {links.map((link) => link)}
+            </ul> */}
         </nav>
     )
 }
---
+++ src/lib/NavData.js
@@ -0,0 +1,22 @@
+export const navData = [
+    {
+        id: 0,
+        text: "Home",
+        link: '/'
+    },
+    {
+        id: 1,
+        text: "Portfolio",
+        link: "portfolio"
+    },
+    {
+        id: 2,
+        text: "Resume",
+        link: "resume"
+    },
+    {
+        id: 3,
+        text: "Contact",
+        link: "contact"
+    }
+]
