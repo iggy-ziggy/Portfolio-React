4050d554d9326859433884e551565259bb7b6fba
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-01-09 13:07:30 -0500

    deleted commented code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import toggle from '../assets/images/toggle.png';
 import portfolioLogo from '../assets/images/portfolio-logo.png';
-import logo from '../assets/images/logo-mh-blue.png'
 
 export default function Nav({ links }) {
     const [open, setOpen] = useState(true);
@@ -14,12 +13,9 @@ export default function Nav({ links }) {
         <nav className="nav collapsible">
             <a href="/"><img className="nav__brand-image" src={portfolioLogo} alt="" /></a>
             <img className='nav__toggler' onClick={toggleOpen} src={toggle} alt="" />
-            <ul className={open ? 'list nav__list collapsible__content collapsible--expanded':'list nav__list collapsible__content nav__list-closed'}>
+            <ul className={open ? 'list nav__list collapsible--expanded':'list nav__list collapsible__content nav__list-closed'}>
                 {links.map((link) => link)}
             </ul>
-            {/* <ul className='list nav__list collapsible__content collapsible--expanded'>
-                {links.map((link) => link)}
-            </ul> */}
         </nav>
     )
 }
