f9fc7feabcc7aa8b77f8ca949d2165ccdb2b54a2
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-01-09 17:07:41 -0500

    added components to homepage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,50 @@
 export default function HomePage() {
     return (
         <div>
-            <h2>Home</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam qui quaerat nobis illum molestias, neque consequatur similique dicta quasi rerum?
-            </p>
+            {/* <!-- Hero --> */}
+    <section className="block block--dark block--skewed-left hero hero--container">
+        <div className="container grid grid--1x2">
+          <header className="hero--header">
+            <h1 className="hero--heading">Matthew Haynes</h1>
+            <h2 className="hero--subtitle">
+              Developer / Artist
+            </h2>
+          </header>
+          <img
+        className="hero__image"
+        src="./src/assets/images/dummy-hero.png"
+      ></img>
+        </div>
+      </section>
+      {/* <!-- Media - about me --> */}
+      <div className="media block block--dark block--skewed about__me">
+        <div className="media--content container grid grid--1x2">
+            <div className="media__image box__box">
+                <div className="box"></div>
+            </div>
+            <div className="media__body">
+              <h3 className="media__title">A Little About Me</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim sit pariatur tenetur recusandae blanditiis. Laboriosam ipsum voluptatem iusto? Veniam itaque totam expedita numquam, ab ad sapiente consequatur eum labore assumenda rerum reprehenderit a, laborum omnis vitae saepe quia nostrum consectetur culpa cum in. Dolore numquam deserunt enim adipisci magnam reiciendis!
+              </p>
+            </div>
+        </div>
+      </div>
+      {/* <!-- Skills --> */}
+      <div className="media block block--dark block--skewed skills">
+        <div className="media--content container grid grid--1x2">
+            <div className="media__body">
+              <h3 className="media__title">Skills</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime tempora
+                recusandae modi iste dolor quam nostrum ad soluta magni perferendis.
+              </p>
+            </div>
+            <div className="media__image box__box">
+                <div className="box"></div>
+            </div>
+        </div>
+      </div>
         </div>
     )
 }
