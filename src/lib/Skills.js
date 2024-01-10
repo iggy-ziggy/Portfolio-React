cce70d338351f7c22ede4d059e28c9a6f8da4b39
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-01-10 12:33:44 -0500

    added files to lib

---
+++ src/lib/Skills.js
@@ -0,0 +1,37 @@
+const skills = [
+  {
+    name: "Object-oriented Programming",
+  },
+  {
+    name: "Problem Solving",
+  },
+  {
+    name: "<another skill>",
+  },
+  {
+    name: "<another skill>",
+  },
+  {
+    name: "<another skill>",
+  },
+  {
+    name: "<another skill>",
+  },
+  {
+    name: "<another skill>",
+  },
+  {
+    name: "<another skill>",
+  },
+  {
+    name: "<another skill>",
+  },
+  {
+    name: "<another skill>",
+  },
+  {
+    name: "<another skill>",
+  },
+];
+
+export default skills;
