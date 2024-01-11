43e52cffd1dbbb833723b59e8b0e12fd3913a1c8
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-01-11 17:22:59 -0500

    added footer and updated social links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer__content">
+                <a href="https://github.com/iggy-ziggy" className="fa fa-github" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://linkedin.com/in/matt-haynes-46b6a7292" className="fa fa-linkedin" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://stackoverflow.com/users/23232410/matthew-haynes" className="fa fa-stack-overflow" target="_blank" rel="noopener noreferrer"></a>
+            </div>
+        </div>
+    )
+}
