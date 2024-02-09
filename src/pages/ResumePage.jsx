bfbd6187dae6b371deb1c4eeaebcfba76b780afd
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-02-09 13:05:18 -0500

    fixed layout issues on small mobile devices

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,11 +1,31 @@
-import ResumeDownload from '../components/ResumeDownload';
-import Skills from '../components/Skills';
+import ResumeDownload from "../components/ResumeDownload";
+import Skills from "../components/Skills";
 
 export default function ResumePage() {
-    return (
-        <>
-            <ResumeDownload />
-            <Skills />
-        </>
-    )
+  return (
+    <>
+      <ResumeDownload />
+      {/* <div className='block block--dark block--skewed'>
+                <p>
+                    I love a challenge. The chance to tackle problems and see them wrestled to the ground is exhilarating. And, occasionally, heartbreaking. Seeing a piece languish in non-functionality is like seeing a friend in need. I just want to help.
+                </p>
+            </div> */}
+      <div className="media block block--dark block--skewed">
+        <div className="media--content container container grid grid--1x2">
+            <div className="media__image icon--vertical-align">
+            <i className="fa-regular fa-lightbulb"></i>
+            </div>
+          <div className="media__body teal-text">
+            <p>
+              I love a challenge. The chance to tackle problems and see them
+              wrestled to the ground is exhilarating. And, occasionally,
+              heartbreaking. Seeing a piece languish in non-functionality is
+              like seeing a friend in need. I just want to help.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Skills />
+    </>
+  );
 }
