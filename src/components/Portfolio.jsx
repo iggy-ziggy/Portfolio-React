3f2f564eb393ebd53b603ba5b5a6a3dc4e3f5e0c
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-03-12 13:22:44 -0400

    cleaned up portfolio images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,38 +1,48 @@
-import React, { Component } from "react";
 import projects from "../lib/Portfolio";
 
 export default function Portfolio() {
   return (
     <div className="portfolio--container container block">
       <div className="grid grid--1x2">
-          {projects.map(function (project, index) {
-            return (
-              <div className="project--card" data-aos="fade-up" data-aos-duration="1000" key={index}>
-                <div className="image__container"><img src={project.image} alt="project image" className="project__image"></img></div>
-                <p className="project__title">{project.title}</p>
-                <div className="btn--container">
-                    <a
-                      href={project.github}
-                      className="project__btn"
-                      style={{ margin: 10 }}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      GitHub
-                    </a>
-                    <a
-                      href={project.deploy}
-                      className="project__btn"
-                      style={{ margin: 10 }}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Deploy
-                    </a>
-                </div>
+        {projects.map(function (project, index) {
+          return (
+            <div
+              className="project--card"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              key={index}
+            >
+              <div className="image__container">
+                <img
+                  src={project.image}
+                  alt="project image"
+                  className="project__image"
+                ></img>
               </div>
-            );
-          })}
+              <p className="project__title">{project.title}</p>
+              <div className="btn--container">
+                <a
+                  href={project.github}
+                  className="project__btn"
+                  style={{ margin: 10 }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+                <a
+                  href={project.deploy}
+                  className="project__btn"
+                  style={{ margin: 10 }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Deploy
+                </a>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/lib/Portfolio.js
+++ src/lib/Portfolio.js
@@ -1,14 +1,23 @@
+import gameHub from '../assets/images/game-hub.png'
+import cloudHosting from '../assets/images/cloud-hosting.png'
+import inDev from '../assets/images/inDev.png'
+import powerGrid from '../assets/images/power-grid.png'
+import socialNetwork from '../assets/images/social-network.png'
+import eCommerce from '../assets/images/e-commerce.png'
+import scheduler from '../assets/images/workday-scheduler.png'
+import passwordGenerator from '../assets/images/password-generator.png'
+import codeQuiz from '../assets/images/code-quiz.png'
 
 const projects = [
     {
         title: 'Game Hub',
-        image: 'https://cdn-icons-png.flaticon.com/256/486/486792.png',
+        image: gameHub,
         github: 'https://github.com/iggy-ziggy/game-hub',
         deploy: 'https://game-hub-wine-ten.vercel.app/'
     },
     {
         title: 'Cloud Hosting Front End',
-        image: 'https://cdn-icons-png.flaticon.com/512/115/115804.png',
+        image: cloudHosting,
         github: 'https://github.com/iggy-ziggy/cloud-hosting-front-end',
         deploy: 'https://cloud-hosting-front-end.netlify.app'
     },
@@ -19,44 +28,44 @@ const projects = [
     //     deploy: 'https://stark-hollows-24341-bcd3c1cc7ff9.herokuapp.com/'
     // },
     {
-        title: 'Artwork Critique App',
-        image: 'https://cdn-icons-png.flaticon.com/512/3659/3659802.png',
+        title: 'inDev',
+        image: inDev,
         github: 'https://github.com/iggy-ziggy/artwork-critique-app',
         deploy: 'https://hidden-crag-39805-685b6677aef9.herokuapp.com/'
     },
     {
         title: 'Power Grid',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlAywbtoaGH14yP-QqXZioWv9z2W1NGYZb5rkUObt6Gob-kGxqF1YcKrhxT0_cj4beRgc&usqp=CAU',
+        image: powerGrid,
         github: 'https://github.com/iggy-ziggy/PowerGrid',
         deploy: 'https://iggy-ziggy.github.io/PowerGrid/'
     },
     {
         title: 'Social Network API',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaudG84aX7-GBXc7PBqIk5yf9pDRly1TIaRg&usqp=CAU',
+        image: socialNetwork,
         github: 'https://github.com/iggy-ziggy/Social-Network-API',
         deploy: 'https://watch.screencastify.com/v/IUmMRwbsLSbKGZWOCFeT'
     },
     {
         title: 'E-Commerce Backend',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0gxYXNEwcT1FebDuMrGjRkBCfQ56pWcwrhg&usqp=CAU',
+        image: eCommerce,
         github: 'https://github.com/iggy-ziggy/e-commerce-back-end',
         deploy: 'https://watch.screencastify.com/v/ugHBaT2o8CNyXhsXe0q0'
     },
     {
         title: 'Workday Scheduler',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO_tvx7tgR4QkJK9vs3y3itY-TuUHPjRPNdw&usqp=CAU',
+        image: scheduler,
         github: 'https://github.com/iggy-ziggy/work-day-scheduler',
         deploy: 'https://iggy-ziggy.github.io/work-day-scheduler/'
     },
     {
         title: 'Random Password Generator',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlFsuqwQqlh_V7M4GtMt2pOohllKcmcldxtw&usqp=CAU',
+        image: passwordGenerator,
         github: 'https://github.com/iggy-ziggy/random-password-generator',
         deploy: 'https://iggy-ziggy.github.io/random-password-generator/'
     },
     {
         title: 'Code Quiz',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTarC4vD5m5YB-p8hwxVjvAxkvjy734WaoAQ&usqp=CAU',
+        image: codeQuiz,
         github: 'https://github.com/iggy-ziggy/code_quiz',
         deploy: 'https://iggy-ziggy.github.io/code_quiz/'
     }
